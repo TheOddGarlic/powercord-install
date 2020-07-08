87c9ff480b5fa12eb3b4b2229b37e3f33b1c9857
TheOddGarlic <theoddgarlic@theoddgarlic.tk>
2020-07-08 13:25:44 +0300

    if windows force close discord.

--- pc-install.sh
+++ pc-install.sh
@@ -3,3 +3,4 @@ git clone https://github.com/powercord-org/powercord &&
 cd powercord &&
 npm i &&
 npm run plug || sudo npm run plug
+echo $MSYSTEM | grep 'MINGW' >/dev/null && powershell -Command Stop-Process -Name "DiscordCanary" -Force # If windows force close DiscordCanary.
